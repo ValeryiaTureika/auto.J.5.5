3dddfa9910e7714e1d292fc0d4902378d4f2fbb3
Valeria Hurava <ler4ik65@gmail.com>
2022-01-04 16:46:30 +0100

    add Allure

--- src/test/java/ru/netology/web/OrderingCardDelivery.java
+++ src/test/java/ru/netology/web/OrderingCardDelivery.java
@@ -1,5 +1,9 @@
 package ru.netology.web;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 import ru.netology.data.DataGenerator;
@@ -10,6 +14,17 @@ import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class OrderingCardDelivery {
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
+
 
     @Test
     public void shouldOrderingCardDelivery() {
